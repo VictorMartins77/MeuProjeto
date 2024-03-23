e264ae683d9e1013474529791f05991421dfa66f
VictorMartins77 <victor3860@outlook.com>
2024-03-23 09:01:03 -0300

    Branch com PopUp

--- Soma.java
+++ Soma.java
@@ -1,8 +1,15 @@
+import javax.swing.JOptionPane;
+
 public class Soma {
     public static void main(String[] args) {
-        int numero1 = 10;
-        int numero2 = 20;
+        String primeiroNumero = JOptionPane.showInputDialog("Digite o primeiro número:");
+        String segundoNumero = JOptionPane.showInputDialog("Digite o segundo número:");
+
+        int numero1 = Integer.parseInt(primeiroNumero);
+        int numero2 = Integer.parseInt(segundoNumero);
+
         int soma = numero1 + numero2;
-        System.out.println("A soma de " + numero1 + " e " + numero2 + " é " + soma);
+
+        JOptionPane.showMessageDialog(null, "A soma de " + numero1 + " e " + numero2 + " é " + soma);
     }
 }
